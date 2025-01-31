f81dbdac304d4c1db3c06159211c2702ab94985c
rishav-bhardwaz <rishav76000@gmail.com>
2025-01-31 22:34:24 +0530

    Fix package functionality issues in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { useAccessibleFocus } from "./hooks/useAccessibleFocus";
 export { checkAccessibility } from "./utils/checkAccessibility";
 export { addAriaAttributes } from "./utils/addAriaAttributes";
+
+
+// the package is not working correctly will fix it as soon as possible
